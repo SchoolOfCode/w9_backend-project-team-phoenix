e2c41a3a77b4085086bf50b61b5b7e0425b5e2d7
min-chih yang <min.chih98@gmail.com>
2022-06-22 11:49:18 +0100

    populate w2-7 data

--- phoenix-backend/lib/data.js
+++ phoenix-backend/lib/data.js
@@ -1,76 +1,76 @@
 export const weeks = [
   {
-    week_number: 1,
-    catagory_id: 1,
+    week_number: 7,
+    catagory_id: 19,
     contact_id: null,
   },
   {
-    week_number: 1,
-    catagory_id: 2,
+    week_number: 7,
+    catagory_id: 20,
     contact_id: null,
   },
   {
-    week_number: 1,
+    week_number: 7,
+    catagory_id: 21,
+    contact_id: null,
+  },
+  {
+    week_number: 7,
     catagory_id: null,
-    contact_id: 1,
+    contact_id: 11,
   },
   {
-    week_number: 8,
-    catagory_id: 3,
-    contact_id: null,
-  },
-  {
-    week_number: 8,
+    week_number: 7,
     catagory_id: null,
-    contact_id: 2,
-  },
+    contact_id: 12,
+  }
 ];
 
 export const catagories = [
   {
-    category_id: 1,
-    catagory_name: "Useful links",
-    title: "The shapes of css",
-    content:
-      "CSS is capable of making all sorts of shapes. Squares and rectangles are easy, as they are the natural shapes of the web. Add a width and height and you have the exact size rectangle you need. Add border-radius and you can round that shape, and enough of it you can turn those rectangles into circles and ovals.",
-    url: "https://css-tricks.com/the-shapes-of-css/",
-    img: null,
-  },
-  {
-    category_id: 2,
-    catagory_name: "Useful links",
-    title: "Git Branch",
-    content:
-      "This document is an in-depth review of the git branch command and a discussion of the overall Git branching model. Branching is a feature available in most modern version control systems. Branching in other VCS's can be an expensive operation in both time and disk space. In Git, branches are a part of your everyday development process. Git branches are effectively a pointer to a snapshot of your changes. When you want to add a new feature or fix a bug—no matter how big or how small—you spawn a new branch to encapsulate your changes. This makes it harder for unstable code to get merged into the main code base, and it gives you the chance to clean up your future's history before merging it into the main branch.",
-    url: "https://www.atlassian.com/git/tutorials/using-branches",
-    img: null,
-  },
-  {
-    category_id: 3,
+    category_id: 19,
     catagory_name: "Docs",
-    title: "React testing",
+    title: "Create React App",
     content:
-      "You want to write maintainable tests for your React components. As a part of this goal, you want your tests to avoid including implementation details of your components and rather focus on making your tests give you the confidence for which they are intended. As part of this, you want your testbase to be maintainable in the long run so refactors of your components (changes to implementation but not functionality) don't break your tests and slow you and your team down.",
-    url: "https://testing-library.com/docs/react-testing-library/intro/",
+      "Create React App is an officially supported way to create single-page React applications. It offers a modern build setup with no configuration.",
+    url: "https://create-react-app.dev/docs/getting-started/",
+    img: null,
+  },
+  {
+    category_id: 20,
+    catagory_name: "Useful Links",
+    title: "Why You Should Use React.js For Web Development",
+    content:
+      "If you are new to React.js, you must be wondering why it is so popular and why you should use it in your applications. If so, then you are in the right place.",
+    url: "https://www.freecodecamp.org/news/why-use-react-for-web-development/",
+    img: null,
+  },
+  {
+    category_id: 21,
+    catagory_name: "Useful Links",
+    title: "Importing and Exporting Components in React",
+    content:
+      "The magic of components lies in their reusability: you can create components that are composed of other components. But as you nest more and more components, it often makes sense to start splitting them into different files. This lets you keep your files easy to scan and reuse components in more places.",
+    url: "https://beta.reactjs.org/learn/importing-and-exporting-components",
     img: null,
   },
 ];
 
 export const contactInfo = [
   {
-    contact_id: 1,
+    contact_id: 11,
     heading: "Contact Information",
-    name: "Chris Meah",
-    twitter: "https://twitter.com/TheMeahCat",
-    linkedin: "https://www.linkedin.com/in/chrismeah/",
-    description: "SOC CEO",
+    name: "Piper Bates",
+    twitter: null,
+    linkedin: "https://www.linkedin.com/in/piperbates//",
+    description: "Junior Software Engineer at Bamboo Auctions",
   },
   {
-    contact_id: 2,
-    name: "Nick Truby",
+    contact_id: 12,
     heading: "Contact Information",
+    name: "David Adams",
     twitter: null,
-    linkedin: "https://www.linkedin.com/in/nicktruby/",
-    description: "React professional in BT",
-  },
+    linkedin: "https://www.linkedin.com/in/aardeedev/",
+    description: "SRE, DevOps, Backend and full-stack JS & PHP engineer",
+  }
 ];
---
+++ phoenix-backend/lib/oldData.js
@@ -0,0 +1,459 @@
+export const weeks = [
+    {
+      week_number: 1,
+      catagory_id: 1,
+      contact_id: null,
+    },
+    {
+      week_number: 1,
+      catagory_id: 2,
+      contact_id: null,
+    },
+    {
+      week_number: 1,
+      catagory_id: null,
+      contact_id: 1,
+    },
+    {
+        week_number: 2,
+        catagory_id: 4,
+        contact_id: null,
+      },
+      {
+        week_number: 2,
+        catagory_id: 5,
+        contact_id: null,
+      },
+      {
+        week_number: 2,
+        catagory_id: 6,
+        contact_id: null,
+      },
+      {
+        week_number: 2,
+        catagory_id: null,
+        contact_id: 3,
+      },
+      {
+        week_number: 2,
+        catagory_id: null,
+        contact_id: 4,
+      },
+      {
+        week_number: 3,
+        catagory_id: 7,
+        contact_id: null,
+      },
+      {
+        week_number: 3,
+        catagory_id: 8,
+        contact_id: null,
+      },
+      {
+        week_number: 3,
+        catagory_id: 9,
+        contact_id: null,
+      },
+      {
+        week_number: 3,
+        catagory_id: null,
+        contact_id: 5,
+      },
+      {
+        week_number: 3,
+        catagory_id: null,
+        contact_id: 6,
+      },
+      {
+        week_number: 4,
+        catagory_id: 10,
+        contact_id: null,
+      },
+      {
+        week_number: 4,
+        catagory_id: 11,
+        contact_id: null,
+      },
+      {
+        week_number: 4,
+        catagory_id: 12,
+        contact_id: null,
+      },
+      {
+        week_number: 4,
+        catagory_id: null,
+        contact_id: 7,
+      },
+      {
+        week_number: 5,
+        catagory_id: 13,
+        contact_id: null,
+      },
+      {
+        week_number: 5,
+        catagory_id: 14,
+        contact_id: null,
+      },
+      {
+        week_number: 5,
+        catagory_id: 15,
+        contact_id: null,
+      },
+      {
+        week_number: 5,
+        catagory_id: null,
+        contact_id: 8,
+      },
+      {
+        week_number: 6,
+        catagory_id: 16,
+        contact_id: null,
+      },
+      {
+        week_number: 6,
+        catagory_id: 17,
+        contact_id: null,
+      },
+      {
+        week_number: 6,
+        catagory_id: 18,
+        contact_id: null,
+      },
+      {
+        week_number: 6,
+        catagory_id: null,
+        contact_id: 9,
+      },
+      {
+        week_number: 6,
+        catagory_id: null,
+        contact_id: 10,
+      },
+      {
+        week_number: 7,
+        catagory_id: 19,
+        contact_id: null,
+      },
+      {
+        week_number: 7,
+        catagory_id: 20,
+        contact_id: null,
+      },
+      {
+        week_number: 7,
+        catagory_id: 21,
+        contact_id: null,
+      },
+      {
+        week_number: 7,
+        catagory_id: null,
+        contact_id: 11,
+      },
+      {
+        week_number: 7,
+        catagory_id: null,
+        contact_id: 12,
+      },
+      {
+        week_number: 8,
+        catagory_id: 3,
+        contact_id: null,
+      },
+      {
+        week_number: 8,
+        catagory_id: null,
+        contact_id: 2,
+      }
+  ];
+  
+  export const catagories = [
+    {
+      category_id: 1,
+      catagory_name: "Useful links",
+      title: "The shapes of css",
+      content:
+        "CSS is capable of making all sorts of shapes. Squares and rectangles are easy, as they are the natural shapes of the web. Add a width and height and you have the exact size rectangle you need. Add border-radius and you can round that shape, and enough of it you can turn those rectangles into circles and ovals.",
+      url: "https://css-tricks.com/the-shapes-of-css/",
+      img: null,
+    },
+    {
+      category_id: 2,
+      catagory_name: "Useful links",
+      title: "Git Branch",
+      content:
+        "This document is an in-depth review of the git branch command and a discussion of the overall Git branching model. Branching is a feature available in most modern version control systems. Branching in other VCS's can be an expensive operation in both time and disk space. In Git, branches are a part of your everyday development process. Git branches are effectively a pointer to a snapshot of your changes. When you want to add a new feature or fix a bug—no matter how big or how small—you spawn a new branch to encapsulate your changes. This makes it harder for unstable code to get merged into the main code base, and it gives you the chance to clean up your future's history before merging it into the main branch.",
+      url: "https://www.atlassian.com/git/tutorials/using-branches",
+      img: null,
+    },
+    {
+      category_id: 3,
+      catagory_name: "Docs",
+      title: "React testing",
+      content:
+        "You want to write maintainable tests for your React components. As a part of this goal, you want your tests to avoid including implementation details of your components and rather focus on making your tests give you the confidence for which they are intended. As part of this, you want your testbase to be maintainable in the long run so refactors of your components (changes to implementation but not functionality) don't break your tests and slow you and your team down.",
+      url: "https://testing-library.com/docs/react-testing-library/intro/",
+      img: null,
+    },
+    {
+        category_id: 4,
+        catagory_name: "Useful links",
+        title: "JavaScript HTML DOM",
+        content:
+          "When a web page is loaded, the browser creates a Document Object Model of the page.",
+        url: "https://www.w3schools.com/js/js_htmldom.asp",
+        img: null,
+      },
+      {
+        category_id: 5,
+        catagory_name: "Useful links",
+        title: "Web APIs - IntroductionGit Branch",
+        content:
+          "API stands for Application Programming Interface. A Web API is an application programming interface for the Web. A Browser API can extend the functionality of a web browser. A Server API can extend the functionality of a web server.",
+        url: "https://www.w3schools.com/js/js_api_intro.asp",
+        img: null,
+      },
+      {
+        category_id: 6,
+        catagory_name: "Docs",
+        title: "Fetch API",
+        content:
+          "The Fetch API provides an interface for fetching resources (including across the network). It will seem familiar to anyone who has used XMLHttpRequest, but the new API provides a more powerful and flexible feature set.",
+        url: "https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API",
+        img: null,
+      },
+      {
+        category_id: 7,
+        catagory_name: "Useful Tools",
+        title: "Miro",
+        content:
+          "The Visual Collaboration Platform for Every Team",
+        url: "https://miro.com/app/",
+        img: null,
+      },
+      {
+        category_id: 8,
+        catagory_name: "Useful Tools",
+        title: "Figma",
+        content:
+          "With Figma, everyone works towards a shared goal. This has enabled our product teams to ship new products faster and feel more confident in their decisions.",
+        url: "https://www.figma.com/",
+        img: null,
+      },
+      {
+        category_id: 9,
+        catagory_name: "Docs",
+        title: "The Guide to Lightweight UX Documentation",
+        content:
+          "UX documentation is a crucial part of the UX design process. It serves as a reference, giving context to the product’s lifespan from the initial concept to the current iteration.",
+        url: "https://www.uxpin.com/studio/blog/3-step-guide-lightweight-ux-documentation/",
+        img: null,
+      },
+      {
+        category_id: 10,
+        catagory_name: "Preparation for Setting Up Your Device",
+        title: "Using a Node Version Manager",
+        content:
+          "There are a lot of different versions of Node out there. These tools will help you keep track of what version you are using, and also make it easy to install new versions and switch between them. They also make npm easier to set up :)",
+        url: "https://npm.github.io/installation-setup-docs/installing/using-a-node-version-manager.html",
+        img: null,
+      },
+      {
+        category_id: 11,
+        catagory_name: "Docs",
+        title: "Express - npm",
+        content:
+          "The Express philosophy is to provide small, robust tooling for HTTP servers, making it a great solution for single page applications, websites, hybrids, or public HTTP APIs. Express does not force you to use any specific ORM or template engine. With support for over 14 template engines via Consolidate.js, you can quickly craft your perfect framework.",
+        url: "https://www.npmjs.com/package/express",
+        img: null,
+      },
+      {
+        category_id: 12,
+        catagory_name: "Useful Tools",
+        title: "Postman API Platform",
+        content:
+          "Postman is an API platform for building and using APIs. Postman simplifies each step of the API lifecycle and streamlines collaboration so you can create better APIs—faster.",
+        url: "https://www.postman.com/",
+        img: null,
+      },
+      {
+        category_id: 13,
+        catagory_name: "Tutorial",
+        title: "SQL Tutorial",
+        content:
+          "SQL is a standard language for storing, manipulating and retrieving data in databases. Our SQL tutorial will teach you how to use SQL in: MySQL, SQL Server, MS Access, Oracle, Sybase, Informix, Postgres, and other database systems.",
+        url: "https://www.w3schools.com/sql/",
+        img: null,
+      },
+      {
+        category_id: 14,
+        catagory_name: "Docs",
+        title: "node-postgres",
+        content:
+          "node-postgres is a collection of node.js modules for interfacing with your PostgreSQL database. It has support for callbacks, promises, async/await, connection pooling, prepared statements, cursors, streaming results, C/C++ bindings, rich type parsing, and more! Just like PostgreSQL itself there are a lot of features: this documentation aims to get you up and running quickly and in the right direction. It also tries to provide guides for more advanced & edge-case topics allowing you to tap into the full power of PostgreSQL from node.js.",
+        url: "https://node-postgres.com/",
+        img: null,
+      },
+      {
+        category_id: 15,
+        catagory_name: "Useful Tools",
+        title: "Heroku: Cloud Application Platform",
+        content:
+          "Heroku is a platform as a service (PaaS) that enables developers to build, run, and operate applications entirely in the cloud.",
+        url: "https://www.heroku.com/",
+        img: null,
+      },
+      {
+        category_id: 16,
+        catagory_name: "Docs",
+        title: "SuperTest",
+        content:
+          "The motivation with this module is to provide a high-level abstraction for testing HTTP, while still allowing you to drop down to the lower-level API provided by superagent.",
+        url: "https://www.npmjs.com/package/supertest",
+        img: null,
+      },
+      {
+        category_id: 17,
+        catagory_name: "Docs",
+        title: "Getting Started - Jest",
+        content:
+          "Jest is a delightful JavaScript Testing Framework with a focus on simplicity.It works with projects using: Babel, TypeScript, Node, React, Angular, Vue and more!",
+        url: "https://jestjs.io/",
+        img: null,
+      },
+      {
+        category_id: 18,
+        catagory_name: "Useful Tools",
+        title: "Cypress - Fast, Easy & Reliable Testing For Anything That Runs In a Browser.",
+        content:
+          "We realized pretty quickly that Cypress was a winner. It combines everything we need into a single tool that feels familiar, but is easy to teach and use. It was VERY well-documented, it had parallel testing, a dashboard for viewing results, in-browser debugging, DOM snapshots, it was in Javascript, and it was super readable. And best of all, the setup is dead simple.",
+        url: "https://go.cypress.io/get-started?utm_adgroup=132501525000&utm_keyword=cypress&utm_source=google&utm_medium=cpc&utm_campaign=15312994475&utm_term=cypress&hsa_acc=8898574980&hsa_cam=15312994475&hsa_grp=132501525000&hsa_ad=562694869893&hsa_src=g&hsa_tgt=kwd-40454352&hsa_kw=cypress&hsa_mt=e&hsa_net=adwords&hsa_ver=3&gclid=CjwKCAjw-8qVBhANEiwAfjXLrhBM0ejPOF07rdQ3RdT1_jBk8P45-TzJYuIZZ8qNr2Vq9ZeWuvxalRoCwawQAvD_BwE",
+        img: null,
+      },
+      {
+        category_id: 19,
+        catagory_name: "Docs",
+        title: "Create React App",
+        content:
+          "Create React App is an officially supported way to create single-page React applications. It offers a modern build setup with no configuration.",
+        url: "https://create-react-app.dev/docs/getting-started/",
+        img: null,
+      },
+      {
+        category_id: 20,
+        catagory_name: "Useful Links",
+        title: "Why You Should Use React.js For Web Development",
+        content:
+          "If you are new to React.js, you must be wondering why it is so popular and why you should use it in your applications. If so, then you are in the right place.",
+        url: "https://www.freecodecamp.org/news/why-use-react-for-web-development/",
+        img: null,
+      },
+      {
+        category_id: 21,
+        catagory_name: "Useful Links",
+        title: "Importing and Exporting Components in React",
+        content:
+          "The magic of components lies in their reusability: you can create components that are composed of other components. But as you nest more and more components, it often makes sense to start splitting them into different files. This lets you keep your files easy to scan and reuse components in more places.",
+        url: "https://beta.reactjs.org/learn/importing-and-exporting-components",
+        img: null,
+      },
+  ];
+  
+  export const contactInfo = [
+    {
+      contact_id: 1,
+      heading: "Contact Information",
+      name: "Chris Meah",
+      twitter: "https://twitter.com/TheMeahCat",
+      linkedin: "https://www.linkedin.com/in/chrismeah/",
+      description: "SOC CEO",
+    },
+    {
+      contact_id: 2,
+      name: "Nick Truby",
+      heading: "Contact Information",
+      twitter: null,
+      linkedin: "https://www.linkedin.com/in/nicktruby/",
+      description: "React professional in BT",
+    },
+    {
+        contact_id: 3,
+        heading: "Contact Information",
+        name: "Joy Lin",
+        twitter: null,
+        linkedin: "https://www.linkedin.com/in/tzuyunlin/",
+        description: "Junior Software Developer at Wise/ Previous SOC Bootcamper",
+      },
+      {
+        contact_id: 4,
+        name: "Katie Hawcutt",
+        heading: "Contact Information",
+        twitter: "https://twitter.com/katie_hawcutt",
+        linkedin: "https://www.linkedin.com/in/katiehawcutt/",
+        description: "Junior Software Engineer at Wise/ Previous SOC Bootcamper",
+      },
+      {
+        contact_id: 5,
+        heading: "Contact Information",
+        name: "Javaid Karim",
+        twitter: null,
+        linkedin: "https://www.linkedin.com/in/javaid-karim-6596a0a0//",
+        description: "Chief Technology Officer at Nester",
+      },
+      {
+        contact_id: 6,
+        name: "Alexander Telecan",
+        heading: "Contact Information",
+        twitter: "null",
+        linkedin: "https://www.linkedin.com/in/alexandertelecan//",
+        description: "Mobile Developer at Nester",
+      },
+      {
+        contact_id: 7,
+        heading: "Contact Information",
+        name: "Liz Kaufman",
+        twitter: "https://twitter.com/codingbumblebee",
+        linkedin: "https://www.linkedin.com/in/liz-kaufman/",
+        description: "SOC Coach/ Previous SOC Bootcamper",
+      },
+      {
+        contact_id: 8,
+        heading: "Contact Information",
+        name: "Kyle Simpson",
+        twitter: "https://twitter.com/getify",
+        linkedin: "https://www.linkedin.com/in/getify/",
+        description: "Author of You Don't Know JS",
+      },
+      {
+        contact_id: 9,
+        heading: "Contact Information",
+        name: "Nadeem Shabir",
+        twitter: null,
+        linkedin: "https://www.linkedin.com/in/nadeemshabir/",
+        description: "Staff Engineer at Talis Group",
+      },
+      {
+        contact_id: 10,
+        heading: "Contact Information",
+        name: "Jordan Powell ",
+        twitter: "https://twitter.com/jordanpowell88",
+        linkedin: "https://www.linkedin.com/in/jpdesigning/",
+        description: "DX Engineer @Cypress",
+      },
+      {
+        contact_id: 11,
+        heading: "Contact Information",
+        name: "Piper Bates",
+        twitter: null,
+        linkedin: "https://www.linkedin.com/in/piperbates//",
+        description: "Junior Software Engineer at Bamboo Auctions",
+      },
+      {
+        contact_id: 12,
+        heading: "Contact Information",
+        name: "David Adams",
+        twitter: null,
+        linkedin: "https://www.linkedin.com/in/aardeedev/",
+        description: "SRE, DevOps, Backend and full-stack JS & PHP engineer",
+      }
+  ];
+  
